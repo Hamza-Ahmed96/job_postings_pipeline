fd66ca9201f01fd5bfc04bae291f8432a7e20dfc
Hamza <hamzaahmedwork96@gmail.com>
2026-03-28 17:20:33 +0000

    feat(dbt) : completed date dimensions table

---
+++ dbt_project/models/dimensions/dims_staging__dim_dates.sql
@@ -0,0 +1,19 @@
+{{config(materialized='table')}}
+
+WITH distinct_dates AS (
+    SELECT DISTINCT
+        CAST(job_posted_date AS DATE) AS date_day
+    FROM {{ref('stg_raw__job_postings')}}
+    WHERE job_posted_date IS NOT NULL
+)
+SELECT
+      TO_CHAR(date_day, 'YYYYMMDD')      AS date_id,
+      date_day,                                                                                                                                        
+      DAYNAME(date_day)                   AS day_name,
+      TO_CHAR(date_day, 'MMMM')          AS month_name,                                                                                                
+      MONTH(date_day)                     AS month_number,                                                                                             
+      YEAR(date_day)                      AS year,
+      WEEKOFYEAR(date_day)                AS week_number                                                                                               
+  FROM distinct_dates
+  ORDER BY date_day                               
+
