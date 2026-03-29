e3ac9fcdfce205df0f44401834293ac7b17ea00c
Hamza <hamzaahmedwork96@gmail.com>
2026-03-29 22:40:00 +0100

    feat(dbt) : added transformation steps to mcp server - Closes #20

--- mcpserver/server.py
+++ mcpserver/server.py
@@ -1,9 +1,8 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from mcp.server.fastmcp import FastMCP
-
+from dbt.cli.main import dbtRunner, dbtRunnerResult
 from ingestion.extract import run_extraction
 import ingestion.load_duckdb
 import ingestion.load_snowflake
@@ -36,18 +35,57 @@ def load_to_snowflake()->str:
     """
     ingestion.load_snowflake.run_load()
     return "Successfully loaded job postings into SnowFlake"
-    
 
 @mcp.tool()
-def run_full_pipeline(countries: list[str], job_titles: list[str]) -> str:
+def run_dbt_models(model: str = "stg_raw__job_postings") -> str:
+    """
+    Run dbt transformation models on Snowflake.
+    Optionally pass a model name to run a specific model and all downstream models.
+    Leave model empty to run all models.
+    Example: model='stg_raw__job_postings' runs staging and everything downstream.
+    """
+    os.chdir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "dbt_project"))
+
+    if model:
+        cmd_params = ["run", "--select", f"{model}+"]
+    else:
+        cmd_params = ["run"]
+
+    dbt = dbtRunner()
+    res: dbtRunnerResult = dbt.invoke(cmd_params)
+
+    if res.success:
+        return "dbt run completed successfully"
+    else:
+        return f"dbt run failed: {res.exception}"
+
+
+@mcp.tool()
+def run_full_pipeline_motherduck(countries: list[str], job_titles: list[str]) -> str:
     """
     Run the full ELT pipeline: fetch jobs from Adzuna and load them into MotherDuck.
     Use this when you want to do both steps in one go. Loads to MotherDuck only.
-    Use fetch_jobs + load_to_snowflake separately if you want to load to Snowflake.
+    Use fetch_jobs + load_to_motherduck separately if you want to load to Snowflake.
     """
     run_extraction(countries=countries, job_titles=job_titles)
     ingestion.load_duckdb.run_load()
     return f"Pipeline complete. Fetched and loaded jobs for {job_titles} in {countries}."
+
+@mcp.tool()
+def run_full_pipeline_snowflake(countries : list[str], job_titles: list[str]) -> str:
+    """ 
+    Run the full ELT pipeline : fetch jobs from Adzuna and load them to Snowflake.
+    Use this when you want to do both steps in one go. Loads to Snowflake only.
+    Use fetch_jobs+ load_to_snowflake.
+    Use run_dbt_models() to run the dbt transformations on snowflake
+    """
+    run_extraction(countries=countries, job_titles=job_titles)
+    ingestion.load_snowflake.run_load()
+    run_dbt_models()
+    return f"Pipeline completed. Fetched and loaded jobs for {job_titles} in {countries}. Transformed the raw data in snowflake with dbt models"
+
+
+
 
 if __name__ == "__main__":
     mcp.run(transport='stdio')
